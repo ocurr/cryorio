f6ed1b3b156de32dc1a3113fc0b847d4c148ce28
Oliver Curry <owcurry42@gmail.com>
2022-01-09 14:22:28 -0700

    added more rio tests

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
--- roborio/roborio_test.go
+++ roborio/roborio_test.go
@@ -5,28 +5,33 @@ package roborio
 import (
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestRoborio(t *testing.T) {
+func getTestRio(t *testing.T) *Roborio {
 	rio, err := NewRoborio(func(user, pass, addr string) (Conn, error) {
 		return &testConn{t.TempDir()}, nil
 	}, "test", "test", Addresses("test"), Team(973), Port(8080))
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	err = rio.Connect()
 	if err != nil {
 		t.Fatal(err)
 	}
+	return rio
+}
 
+func TestRoborio(t *testing.T) {
 	t.Run("CreateAndSeeFiles", func(t *testing.T) {
+		rio := getTestRio(t)
 		out, err := rio.Touch("foo.txt")
 		if err != nil {
 			t.Fatal(err)
 		}
 		if len(out) != 0 {
-			t.Fatal(err)
+			t.Fatal(out)
 		}
 		out, err = rio.ListDir()
 		if err != nil {
@@ -35,6 +40,58 @@ func TestRoborio(t *testing.T) {
 		got := strings.TrimSpace(string(out))
 		if got != "foo.txt" {
 			t.Fatalf("want: foo.txt got: %s", got)
+		}
+	})
+	t.Run("CreateAndDeleteFiles", func(t *testing.T) {
+		rio := getTestRio(t)
+		out, err := rio.Touch("bar.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(out) != 0 {
+			t.Fatal(out)
+		}
+		out, err = rio.Remove("bar.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(out) != 0 {
+			t.Fatal(out)
+		}
+		out, err = rio.ListDir()
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := strings.TrimSpace(string(out))
+		if got != "" {
+			t.Fatalf("want: %q got: %s", "", got)
+		}
+	})
+	t.Run("CopyFiles", func(t *testing.T) {
+		rio := getTestRio(t)
+		out, err := rio.Touch("FRCUserProgram")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(out) != 0 {
+			t.Fatal(out)
+		}
+		out, err = rio.Copy("FRCUserProgram", "match.backup")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(out) != 0 {
+			t.Fatal(out)
+		}
+		out, err = rio.ListDir()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotStr := strings.TrimSpace(string(out))
+		got := strings.Split(gotStr, "\n")
+		want := []string{"FRCUserProgram", "match.backup"}
+		if !cmp.Equal(got, want) {
+			t.Fatalf("want: %q got: %q", want, got)
 		}
 	})
 }
